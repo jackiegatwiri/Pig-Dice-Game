597376e2695b5a36985baade216551e60cbdc48c
jackiegatwiri <jaciegatz@gmail.com>
2019-03-05 13:47:43 +0300

    add Prototype roll

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@ function Players(name)
   this.tempScore = 0;
   this.tempInstance = 0;
 }
+Players.prototype.roll = function()
+{
+  var random = Math.floor(Math.random() * 6) + 1;
+  this.tempScore += random;
+  this.tempInstance = random;
+}
